415c314d980b0e404a9311aa84297835433af20f
Eddie Meyer <edmeyer8851@gmail.com>
2023-02-05 16:11:51 -0500

    commit tree bb3c895ac58fea8f1e46f51dd191bcef516c77d9

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,36 @@
 class ItemsController < ApplicationController
 
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    render json: Item.find(params[:id])
+  end
+
+  def create
+      user = User.find(params[:user_id])
+      new_item = Item.create(item_params)
+      user.items << new_item
+      render json: new_item, status: :created
+  end
+
+  private
+
+  def render_not_found
+    render json: { error: "User not found" }, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
   end
 
 end
